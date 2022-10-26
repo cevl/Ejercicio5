2717f0781087479f0cf1de52066e2bbd9af11ccc
César Emilio Vásquez Lugo <cevl960504@gmail.com>
2022-10-26 04:45:07 -0400

    Initial Commit

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+resource "digitalocean_droplet" "vm" {
+    image = "ubuntu-20-04-x64"
+    name = "vm"
+    region = "nyc3"
+    size = "s-1vcpu-1gb"
+    ssh_keys = [
+        data.digitalocean_ssh_key.terraform.id
+    ]
+    
+    connection {
+        host = self.ipv4_address
+        user = "root"
+        type = "ssh"
+        private_key = file(var.pvt_key)
+        timeout = "2m"
+    }
+    
+    provisioner "remote-exec" {
+        inline = [
+            "export PATH=$PATH:/usr/bin",
+            "sudo apt update",
+            "sudo apt install -y git docker",
+            "sudo apt install -y docker-compose",
+            "mkdir app",
+            "cd app",
+            "git clone https://github.com/cevl/algo3 .",
+            "docker-compose -f docker-compose.yml up --build"
+        ]
+    }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "do_token" {
+    default = "dop_v1_d524c0ac95fdcec8ba06999e6f967394ccf44b9828c34e5f2030cd87f8a01366"
+}
+
+variable "pvt_key" {
+    default = "pv_ssh.key"
+}
