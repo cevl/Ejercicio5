b4b2a6224af9b437473ba9c4ff50b40bf4944004
César Emilio Vásquez Lugo <cevl960504@gmail.com>
2022-10-30 01:25:17 -0400

    Changed access token again

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "do_token" {
-    default = "dop_v1_72f4dd21ac1d99920bbb88e1efd6145447ec949fb5d9792d2bc5ad5cf18e8544"
+    default = "dop_v1_85fe38f8f26b6d3ef7f7c8e6c68da8010124cb9eb829771a0b91a7743725ca3d"
 }
 
 variable "pvt_key" {
